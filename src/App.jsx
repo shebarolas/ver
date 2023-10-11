2336ac157ba34f7b91f85e9b95c906e33cfe2c2f
shebarolas <30813367+shebarolas@users.noreply.github.com>
2023-10-10 23:22:17 -0300

    ojala

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <iframe src="https://rudo.video/live/c13?c3=13CL&autostart=1&volume=0&tag=eyJwcmUiOiJodHRwczpcL1wvcHViYWRzLmcuZG91YmxlY2xpY2submV0XC9nYW1wYWRcL2Fkcz9pdT1cLzExMjM3MjIwN1wvZW52aXZvLmNsXC9lbnZpdm9cL3ByZXJvbGwmZGVzY3JpcHRpb25fdXJsPWh0dHAlM0ElMkYlMkZ3d3cuMTMuY2wlMkZlbi12aXZvJmVudj12cCZpbXBsPXMmY29ycmVsYXRvcj0mdGZjZD0wJm5wYT0wJmdkZnBfcmVxPTEmb3V0cHV0PXZhc3Qmc3o9NjU2eDM2OCZjaXVfc3pzPTcyOHg5MCw5NzB4OTAsOTcweDI1MCwxOTAweDgyNSwxOTIweDc2OCw0MDB4NjAmdW52aWV3ZWRfcG9zaXRpb25fc3RhcnQ9MSZwbW5kPTAmcG14ZD0xMjAwMDAmcG1hZD0yJnBvZD0xIiwicG9zdCI6MCwibWlkIjp7InRhZyI6MCwidGltZSI6W119LCJvdmVyIjp7InRhZyI6Imh0dHBzOlwvXC9wdWJhZHMuZy5kb3VibGVjbGljay5uZXRcL2dhbXBhZFwvYWRzP2l1PVwvMTEyMzcyMjA3XC9lbnZpdm8uY2xcL2Vudml2b1wvb3ZlcmxheSZkZXNjcmlwdGlvbl91cmw9aHR0cCUzQSUyRiUyRnd3dy4xMy5jbCUyRmVuLXZpdm8mZW52PXZwJmltcGw9cyZjb3JyZWxhdG9yPSZ0ZmNkPTAmbnBhPTAmZ2RmcF9yZXE9MSZvdXRwdXQ9dmFzdCZ2YWRfdHlwZT1ub25saW5lYXImc3o9NDAweDYwJm1pIiwidGltZSI6WzYwXX19
+      " frameborder="0"
+      allow="autoplay; fullscreen; encrypted-media" width="600" height="600"
+      ></iframe>
+    </>
+  )
+}
+
+export default App
